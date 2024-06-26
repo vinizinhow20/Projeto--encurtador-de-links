6af420c9a5db597396c7307cac52d318e1985d88
Vinicius Gabriel <viniciusxisto2014@hotmail.com>
2024-06-26 13:52:19 -0300

    commit inicial

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import express from 'express'
+import 'dotenv/config' 
+const app = express()
+
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    return res.json("Tudo Certo.")
+})
+
+app.listen(process.env.PORT,() => {
+    console.log(`Servidor Funcionando Na Porta ${process.env.PORT}.`)
+})
